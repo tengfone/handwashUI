9e8f2dbf065f548625866ff413f1a37733c7e337
tengfone <phangtf@gmail.com>
2021-04-24 00:56:04 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Handwash
+
+## Installation
+Either run it locally or on an AWS EC2 Instance (XLarge with 32GB Disk Storage on Ubuntu LTS 20.X). You will need to have the following packages found under Technologies to run.
+
+
+```npm install```
+```npm run dev```
---
+++ src/client/App.js
@@ -0,0 +1,115 @@
+import React, { Component } from 'react';
+import './app.css';
+import FileUpload from './fileupload';
+import VideoPlayer from './videoplayer';
+import { Dropdown, Button } from 'react-bootstrap';
+import axios from 'axios';
+import LoadingOverlay from 'react-loading-overlay';
+
+
+export default class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      uploadedFiles: [],
+      videoUrl: "",
+      pythonOutput: "",
+      isLoading: false
+    };
+  }
+
+  componentDidMount() {
+    this.getLocalFileNames();
+  }
+
+  getLocalFileNames() {
+    axios.get('http://0.0.0.0:8080/api/getFileName', {
+    }).then(res => {
+      this.setState({
+        uploadedFiles: res.data.allFilesName
+      })
+    }).catch(err => console.log(err))
+  }
+
+  handleDropdown(chosenFile) {
+    this.setState((state) => {
+      return { videoUrl: chosenFile }
+    });
+  }
+
+  processSelect(chosenFile) {
+    this.setState((state) => {
+      return { isLoading: true }
+    });
+    axios.get('http://0.0.0.0:8080/api/processFile', {
+      params: {
+        fileName: chosenFile
+      }
+    }).then(res => {
+      this.setState({
+        pythonOutput: res.data.mloutput,
+        isLoading: false
+      })
+    }).catch(err => console.log(err))
+  }
+
+  render() {
+    return (
+      <div>
+        <style>
+          @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+          </style>
+        <a href="./"><h1>Do The 7 Steps Hand Wash!</h1></a>
+        <body>
+          <FileUpload />
+        </body>
+        <div style={{ backgroundColor: "white", justifyContent: "center" }}>
+          <section id='section2'>
+            <div className="dropdown" style={{ textAlign: 'center', backgroundColor: 'orange' }}>
+              <Dropdown>
+                <Dropdown.Toggle
+
+                  variant="secondary btn-sm"
+                  id="dropdown-basic">
+                  Choose Your File
+        </Dropdown.Toggle>
+                <Dropdown.Menu style={{ backgroundColor: '#73a47' }}>
+                  {this.state.uploadedFiles.map(({ name, value }) => (
+                    <Dropdown.Item key={name} value={value} onSelect={() => { this.handleDropdown(value) }} >{name}</Dropdown.Item>
+                  ))}
+                </Dropdown.Menu>
+              </Dropdown>
+            </div>
+          </section>
+          <section id="section99">
+            {this.state.videoUrl &&
+              <div style={{ display: "flex", justifyContent: 'flex-end' }}>
+                <VideoPlayer videoUrl={this.state.videoUrl} />
+                <Button onClick={() => { this.processSelect(this.state.videoUrl) }} variant="warning">Process</Button>
+              </div>
+            }
+          </section>
+          <section id='section1' style={{ textAlign: 'center', backgroundColor: 'orange' }}>
+            {
+              this.state.isLoading ? <LoadingOverlay style={{justifyContent:'center',display:'flex'}}
+                active={this.state.isLoading}
+                spinner
+              >
+                <p>Processing Your Model....</p>
+              </LoadingOverlay> : <div/>
+            }
+            {this.state.pythonOutput &&
+              <div style={{ display: "flex", justifyContent: 'flex-end' }}>
+                <h2>{this.state.pythonOutput}</h2>
+              </div>
+            }
+          </section>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+
