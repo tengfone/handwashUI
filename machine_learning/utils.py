c86119bdb803984b26d11f0f45729cc088695966
tengfone <phangtf@gmail.com>
2021-05-09 20:18:29 +0800

    updates

--- machine_learning/utils.py
+++ machine_learning/utils.py
@@ -4,7 +4,6 @@ import torch.nn as nn
 import numpy as np
 import cv2
 import datetime
-import random
 
 def video_to_3d(filename, img_size=(128,128)):
     """
@@ -47,60 +46,10 @@ def video_to_3d(filename, img_size=(128,128)):
 
     return np.array(framearray)
 
-def train(model, device, train_loader, val_loader, optimizer, epoch):
-    """
-    Trains the model on training data
-    """
-    model.train()
-    for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
-
-        # convert one-hot to numerical categories
-        target = torch.argmax(target, dim=1).long()
-        optimizer.zero_grad()
-        output = model(data)
-        criterion = nn.CrossEntropyLoss()
-        loss = criterion(output, target)
-        loss.backward()
-        optimizer.step()
-
-    train_loss, train_acc = evaluate(model, device, train_loader)
-    print('Train Epoch: {} @ {} \nTrain Loss: {:.4f} - Train Accuracy: {:.1f}%'.format(
-        epoch, datetime.datetime.now().time(), train_loss, train_acc))
-
-    val_loss, val_acc = evaluate(model, device, val_loader)
-    print("Val Loss: {:.4f} - Val Accuracy: {:.1f}%".format(val_loss, val_acc))
-
-    return train_loss, train_acc, val_loss, val_acc
-
-
-def evaluate(model, device, data_loader):
-    """
-    Evaluates the model and returns loss, accuracy
-    """
-    model.eval()
-    loss = 0
-    correct = 0
-    with torch.no_grad():
-        for data, target in data_loader:
-            data, target = data.to(device), target.to(device)
-            target = torch.argmax(target, dim=1).long()
-            output = model(data)
-            criterion = nn.CrossEntropyLoss()
-            loss += criterion(output, target)
-            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-            correct += pred.eq(target.view_as(pred)).sum().item()
-
-    loss /= len(data_loader)
-    acc = 100. * correct / len(data_loader.dataset)
-
-    return loss, acc
-
-def predict(model, device, video_path, num_frames = 16):
+def predict(model, video_path, num_frames = 16):
     """
     Predicts the label of the video given its filepath
     """
-
     arr = video_to_3d(video_path)
     
     # randomly select time index for temporal jittering
@@ -109,14 +58,13 @@ def predict(model, device, video_path, num_frames = 16):
     # Crop and jitter the video using indexing
     # The temporal jitter takes place via the selection of consecutive frames
     arr = arr[time_index:time_index + num_frames,:,:,:]
-    
+
     # resize
-    arr = np.array([cv2.resize(img, (64,64)) for img in arr])
     arr = np.asarray(arr) / 255
     arr = arr.transpose(0, 3, 1, 2)
     arr = np.expand_dims(arr, axis=0)
     arr = torch.from_numpy(arr).float()
-        
+
     output = model(arr)
     pred = output.argmax(dim=1, keepdim=True).item()
     classes = {0: 'Step 1',
